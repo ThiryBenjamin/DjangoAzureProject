0a8e569f0040f0f3368cc82a1dd0d9e559bd697c
Benjamin Thiry <benjamin.thiry@qbere.com>
2023-04-06 09:06:16 +0200

    Project initialization

---
+++ polls/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello world, you're finaly able to deploy an app in Azure!")
